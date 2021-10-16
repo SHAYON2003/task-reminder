a8d6d76a224539fd26a7dcc0f2d7e9fc29738e41
coolsunny <89565209+SHAYON2003@users.noreply.github.com>
2021-10-16 18:04:35 +0530

    Create task reminder.py

---
+++ task reminder.py
@@ -0,0 +1,10 @@
+# small code
+import time
+reminder = str(input('Enter a task to remind you at a particular time:")
+ti = float(input("Enter time: "))
+ti*=60
+time.sleep(ti)           #sleep method/function
+
+
+print(reminder)        #this task will execute after sleep method
+
